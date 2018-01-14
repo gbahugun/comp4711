be43185b77c7bceece44bd7deb4b3e80b1a5eb07
gbahugun <gaganbahugun@hotmail.com>
2018-01-14 12:21:20 -0800

    added comments

--- Student.php
+++ Student.php
@@ -1,18 +1,15 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Student
+ * Creates a student that has a surname, a first name, email addresses, and grades.
+ * Also gets the average grade of the student and creates a string describing the
+ * student.
  *
  * @author gagan
  */
 class Student {
 
+    //Constructor for student class
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,14 +17,27 @@ class Student {
         $this->grades = array();
     }
 
+    /*
+     * Adds email to student
+     * Params: which - type of email
+     *         address - email address
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
-
+    
+    /*
+     * Adds grade to student
+     * Params: grade - the students grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /*
+     * Gets the average grade of the student
+     * Return: returns the average grade
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -35,6 +45,10 @@ class Student {
         return $total / count($this->grades);
     }
 
+    /*
+     * Creates a string containing the students full name, average, and emails
+     * Return: a string describing the student
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' (' . $this->average() . ")\n";
